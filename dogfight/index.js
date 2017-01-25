7b3202b19f0db7a3a9b90c834dc2ff48e7232275
Martin Naumann <martin@geekonaut.de>
2017-01-25 07:45:26 +0100

    Added networking to server

--- dogfight/index.js
+++ dogfight/index.js
@@ -14,6 +14,38 @@ var scores = {};
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received:', message, 'player', ws.playerName);
+    var msg = JSON.parse(message)
+    switch(msg.type) {
+      case 'JOIN':
+        if(scores[msg.name]) {
+          ws.playerName = msg.name + Math.round(Math.random() * 100)
+        } else {
+          ws.playerName = msg.name
+        }
+        scores[ws.playerName] = 0
+      break
+      case 'POSITION':
+        wss.clients.forEach(function(sock) {
+          if(sock.playerName === ws.playerName) return
+          sock.send(msg)
+        })
+      break
+      case 'SCORE':
+        scores[ws.playerName]++;
+        wss.clients.forEach(function(sock) {
+          sock.send(msg)
+          sock.send(JSON.stringify({
+            type: 'GOALPOSITION',
+            position: [ Math.random() * 1000 - 500, Math.random() * 100 + 50, Math.random() * 1000 - 500 ]
+          }))
+        })
+      break
+      case 'ADMIN.END':
+        wss.clients.forEach(function(sock) {
+          sock.send(msg)
+        })
+      break
+    }
   })
 
 })
